8db34ccb9375275c13000d45532808d2ea4fc584
David Mortkowitz and Regina Nurieva <regina@>
2018-08-06 16:40:25 -0700

    Add initial JavaScript for a simple cipher webapp.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var sentence = prompt("Please enter a sentence.")
+
+
+
+var firstletter = sentence.charAt(0).toUpperCase();
+var lastletter = sentence.charAt(sentence.length-1).toUpperCase();
+
+alert("first: "+firstletter)
+alert("last: "+lastletter)
+
+
+
+alert(firstletter + lastletter);
